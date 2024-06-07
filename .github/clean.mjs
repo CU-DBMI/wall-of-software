d8f273dff8d296ef921d848d93442d02e3274f29
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-06-07 12:09:20 -0400

    cleanup

--- .github/clean.mjs
+++ .github/clean.mjs
@@ -1,4 +1,3 @@
-import { readdirSync, unlinkSync } from "fs";
 import { ExifTool } from "exiftool-vendored";
 
 const exiftool = new ExifTool();
@@ -10,10 +9,9 @@ await exiftool.end();
 
 // strip all metadata from all images in folder to ensure consistency
 async function stripMeta(folder) {
-  await exiftool.deleteAllTags(folder, ["-recurse", "-overwrite_original"]);
-  // delete originals
-  readdirSync(folder)
-    .filter((filename) => filename.endsWith("_original"))
-    .map((filename) => `${folder}/${filename}`)
-    .forEach(unlinkSync);
+  await exiftool.write(folder, {}, [
+    "-All=",
+    "-recurse",
+    "-overwrite_original",
+  ]);
 }
